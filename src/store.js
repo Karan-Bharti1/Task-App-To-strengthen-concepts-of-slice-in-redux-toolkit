52e7f9283b8a9572b4e4b3807691e8dabee42e40
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-06 10:16:18 +0530

    feat: configured store for tasksSlice

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksSlice from "./features/tasks/tasksSlice";
+
+export default configureStore({reducer:{
+    tasks:tasksSlice.reducer
+}})
